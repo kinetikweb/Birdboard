df690b475f9cf04f9cbaee39c614ef0481a3ba74
kinetik <kinetik2017@gmail.com>
2019-06-14 17:31:51 +0300

    Touch it

--- app/Task.php
+++ app/Task.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
+    
+    protected $touches = ['project'];
 
     public function project()
     {
