3879d7680050c6128441f796cd3a08a10dccbab1
kinetik <kinetik2017@gmail.com>
2019-06-17 18:40:38 +0300

    Project Activity Feeds: Part 2

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -13,7 +13,7 @@ class TaskTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_belongs_to_a_project()
+    function it_belongs_to_a_project()
     {
         $task = factory(Task::class)->create();
 
@@ -21,10 +21,22 @@ class TaskTest extends TestCase
     }
 
     /** @test */
-    public function it_has_a_path()
+    function it_has_a_path()
     {
         $task = factory(Task::class)->create();
 
         $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+    }
+
+    /** @test */
+    function it_can_be_completed()
+    {
+        $task = factory(Task::class)->create();
+
+        $this->assertFalse($task->completed);
+
+        $task->complete();
+
+        $this->assertTrue($task->fresh()->completed);
     }
 }
