80e95514eccd3939ef4a4e6b23db2cc4e0bf9e78
kinetik <kinetik2017@gmail.com>
2019-06-18 18:35:50 +0300

    The Subject of the Activity

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Tests\TestCase;
 use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -38,7 +39,12 @@ class TriggerActivityTest extends TestCase
         $project->addTask('Some task');
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some task', $activity->subject->body);
+        });
     }
 
     /** @test */
@@ -53,7 +59,11 @@ class TriggerActivityTest extends TestCase
             ]);
 
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
@@ -87,7 +97,7 @@ class TriggerActivityTest extends TestCase
         $project = ProjectFactory::withTasks(1)->create();
 
         $project->tasks[0]->delete();
-        
+
         $this->assertCount(3, $project->activity);
     }
 }
