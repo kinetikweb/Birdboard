5f77923cd41d6059c45377ccd48b34732a983cc7
kinetik <kinetik2017@gmail.com>
2019-06-19 21:13:02 +0300

    Authorized Users Can Delete Projects

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,7 +2,15 @@
     <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-102 pl-4">
         <a href="{{ $project->path() }}" class="text-black">{{ $project->title }}</a>
     </h3>
-    <div class="text-gray-700">
+    <div class="text-gray-700 mb-4">
         {{ str_limit($project->description, 100) }}
     </div>
+
+    <footer>
+        <form class="text-right" action="{{ $project->path() }}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="text-xs">Delete</button>
+        </form>
+    </footer>
 </div>
