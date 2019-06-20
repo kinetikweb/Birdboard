ae09aa52d70115355a69ab07d530ae69506b7ec6
kinetik <kinetik2017@gmail.com>
2019-06-20 13:39:19 +0300

    Beware False Positives

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -17,5 +18,26 @@ class UserTest extends TestCase
         $user = factory('App\User')->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+        $nick = factory(User::class)->create();
+
+        $project = tap(ProjectFactory::ownedBy($sally)->create())->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
