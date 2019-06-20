50525fbe520add3924b4e6c6981c10a8dcb0a28e
kinetik <kinetik2017@gmail.com>
2019-06-20 20:18:08 +0300

    Validation Errors For Multiple Forms

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,8 +1,8 @@
-<div class="card" style="height: 200px">
+<div class="card flex flex-col" style="height: 200px">
     <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-102 pl-4">
         <a href="{{ $project->path() }}" class="text-black">{{ $project->title }}</a>
     </h3>
-    <div class="text-gray-700 mb-4">
+    <div class="text-gray-700 mb-4 flex-1">
         {{ str_limit($project->description, 100) }}
     </div>
 
